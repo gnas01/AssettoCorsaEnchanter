b9d8fe3722f5e1117eb2f68a5ae98406025dfc3f
gNash-dev <gNash-dev@protonmail.com>
2023-01-24 14:38:50 +0200

    clear process cache when entering waitingstate

--- src/waiting_state.cpp
+++ src/waiting_state.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include "asseto_corsa_procmon.h"
 #include "running_state.h"
+#include <thread>
+
 
 void WaitingState::Enter()
 {
+	assetoCorsa->ClearCache();
 	std::cout << "Waiting for AssetoCorsa" << std::endl;
 }
 
